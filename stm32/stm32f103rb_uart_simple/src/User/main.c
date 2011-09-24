e3fc3afc42677818545d2dec96d35de321d02b40
yus.projectproto <yus.projectproto@gmail.com>
2011-09-24 04:09:14 +0000

    outline for uart initialization

--- stm32/stm32f103rb_uart_simple/src/User/main.c
+++ stm32/stm32f103rb_uart_simple/src/User/main.c
@@ -1,13 +1,27 @@
-/*******************************
- * project: STM32F10X Template
+/**********************************
+ * project: STM32F10X Simple UART
  *    file: main.c
- *******************************/
+ **********************************/
 
 #include "stm32f10x.h"
 
+void UART1_init(void);
+
 int main()
 {
-
+	UART1_init();
 
 	return 0;
+}
+
+void UART1_init()
+{
+	// TODO
+	// 1. RCC configuration - enable clocks on peripherals
+
+	// 2. GPIO configuration - configure RX and TX pin modes
+
+	// 3. USART configuration - configure serial port settings
+
+	// 4. finally, enable USART peripheral
 }
