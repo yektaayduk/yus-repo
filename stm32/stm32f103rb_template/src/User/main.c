e85e9b960cbafd71ba9b92474d24d5a676d3306d
yus.projectproto <yus.projectproto@gmail.com>
2011-09-24 02:04:40 +0000

    added initial project template for stm32f103rb (STM32F10x_MD)

---
+++ stm32/stm32f103rb_template/src/User/main.c
@@ -0,0 +1,13 @@
+/*******************************
+ * project: STM32F10X Template
+ *    file: main.c
+ *******************************/
+
+#include "stm32f10x.h"
+
+int main()
+{
+
+
+	return 0;
+}
