c5c8cd30ef257be7716a3117041cd38c1fd379bc
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2011-09-24 13:53:02 +0000

    outline for initialization of uart for interrupt mode

--- trunk/stm32/stm32f103rb_uart_interrupt/src/User/main.c
+++ trunk/stm32/stm32f103rb_uart_interrupt/src/User/main.c
@@ -5,9 +5,25 @@
 
 #include "stm32f10x.h"
 
+void uart_init(void);
+
 int main()
 {
-
+	uart_init();
 
 	return 0;
+}
+
+void uart_init()
+{
+	// TODO's
+	// 1. RCC Configuration - enable peripheral clocks
+
+	// 2. GPIO Configuration - configure RX and TX pins
+
+	// 3. USART Configuration - serial port settings
+
+	// 4. NVIC Configuration - enable uart interrupt
+
+	// 5. finally, enable uart peripheral
 }
