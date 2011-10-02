8f336f7c6e8617053e15c1d53d936abd41e14a92
yus.projectproto <yus.projectproto@gmail.com>
2011-10-02 08:04:56 +0000

    import "/trunk/stm32/stm32f103rb_blinker" which will be modified for the FreeRTOS project template

---
+++ FreeRTOS/STM32/stm32f103rb_freertos_template/src/User/main.c
@@ -0,0 +1,41 @@
+/*******************************
+ * project: STM32 LED Blinker
+ *    file: main.c
+ *******************************/
+
+#include "stm32f10x.h"
+
+/* led connected to a gpio pin */
+#define LED_PIN    GPIO_Pin_9
+#define LED_PORT   GPIOB
+
+/* user functions */
+void delay(unsigned long count);
+
+int main()
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    /* enable clock on GPIOB peripheral */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    /* set pin output mode */
+    GPIO_InitStructure.GPIO_Pin = LED_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(LED_PORT, &GPIO_InitStructure);
+
+    while(1)
+    {
+        GPIO_SetBits(LED_PORT, LED_PIN);	// set pin high
+        delay(200000);
+        GPIO_ResetBits(LED_PORT, LED_PIN);	// set pin low
+        delay(200000);
+    }
+    return 0;
+}
+
+void delay(unsigned long count)
+{
+    while(count--);
+}
