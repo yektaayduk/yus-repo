925c4432a28f21cb35d455ee864fb20aea2aa7f3
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2012-02-18 01:14:51 +0000

    CoOS: initial testing of all servos

--- trunk/Bots/quad_lpc1114/src/user/main.c
+++ trunk/Bots/quad_lpc1114/src/user/main.c
@@ -6,19 +6,24 @@
 
 #include "gpio.h"
 #include "xuart.h"
+#include "servo.h"
 #include "quad.h"
 
 #define PRIORITY_BLINK_TASK  2		/* Priority of Blink task */
 #define PRIORITY_PRINT_TASK  2		/* Priority of Blink task */
+#define PRIORITY_SERVOTEST_TASK  3
 
 #define SIZE_BLINK_TASK	  100    /* Stack size of Blink task */
 #define SIZE_PRINT_TASK	  200    /* Stack size of Blink task */
+#define SIZE_SERVOTEST_TASK	  200
 
 OS_STK stkBlink[SIZE_BLINK_TASK];	/* Stack of Blink task */
 OS_STK stkPrint[SIZE_PRINT_TASK];	/* Stack of Blink task */
+OS_STK stkServoTest[SIZE_SERVOTEST_TASK];
 
 void taskBlink(void *param);
 void taskPrint(void *param);
+void taskServoTest(void *param);
 
 int main(void)
 {
@@ -31,12 +36,22 @@ int main(void)
 			      &stkBlink[SIZE_BLINK_TASK-1],
 			      SIZE_BLINK_TASK );
 
+#if 0
 	/* Create Print Task */
 	CoCreateTask( taskPrint,
 				  (void *)0,
 				  PRIORITY_PRINT_TASK,
 				  &stkPrint[SIZE_PRINT_TASK-1],
 				  SIZE_PRINT_TASK );
+#endif
+
+#if 1
+	CoCreateTask( taskServoTest,
+				  (void *)0,
+				  PRIORITY_SERVOTEST_TASK,
+				  &stkPrint[SIZE_SERVOTEST_TASK-1],
+				  SIZE_SERVOTEST_TASK );
+#endif
 
 	CoStartOS();
 
@@ -70,6 +85,31 @@ void taskPrint(void *param)
 	for(;;) {
 		printf("hello, world! (%ld) \n", ++cnt);
 		CoTickDelay(50);	/* Delay 500ms */
+	}
+}
+
+void taskServoTest(void *param)
+{
+	quad_init();
+	uint16_t angle, pwm;
+
+	for(;;) {
+		for(angle=30; angle<150; angle++){
+			pwm = ANGLE_TO_COUNTER(angle);
+			servo_pwm[2] = pwm;
+			servo_pwm[5] = pwm;
+			servo_pwm[8] = pwm;
+			servo_pwm[11] = pwm;
+			CoTickDelay(2);
+		}
+		for(angle=150; angle>30; angle--){
+			pwm = ANGLE_TO_COUNTER(angle);
+			servo_pwm[2] = pwm;
+			servo_pwm[5] = pwm;
+			servo_pwm[8] = pwm;
+			servo_pwm[11] = pwm;
+			CoTickDelay(2);
+		}
 	}
 }
 
