1a76432075edbea715b82ab7b0187a69da20d7ae
yus.projectproto <yus.projectproto@gmail.com>
2012-04-28 15:17:30 +0000

    apply updates from OpenCV-2.3.1 ro OpenCV-2.4.0-beta

--- Android/apps/objtrack/jni/Android.mk
+++ Android/apps/objtrack/jni/Android.mk
@@ -2,14 +2,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-OPENCV_CAMERA_MODULES := off
+OPENCV_LIB_TYPE        := STATIC
+OPENCV_INSTALL_MODULES := on
+OPENCV_CAMERA_MODULES  := off
 
-OPENCV_MK_PATH := ../../OpenCV-2.3.1/share/OpenCV
+OPENCV_MK_PATH := ../../OpenCV-2.4/share/OpenCV
 
 include $(OPENCV_MK_PATH)/OpenCV.mk
 
 LOCAL_MODULE    := objtrack_opencv_jni
 LOCAL_SRC_FILES := objtrack.cpp
-LOCAL_LDLIBS +=  -llog -ldl
+LOCAL_LDLIBS    +=  -llog -ldl
 
 include $(BUILD_SHARED_LIBRARY)
--- Android/apps/objtrack/src/org/projectproto/objtrack/ObjTrackView.java
+++ Android/apps/objtrack/src/org/projectproto/objtrack/ObjTrackView.java
@@ -5,26 +5,46 @@ import android.graphics.Bitmap;
 
 class ObjTrackView extends SampleViewBase {
 
+	private int mFrameSize;
+	private Bitmap mBitmap;
+	private int[] mRGBA;
+
     public ObjTrackView(Context context) {
         super(context);
     }
 
+	@Override
+	protected void onPreviewStared(int previewWidtd, int previewHeight) {
+		mFrameSize = previewWidtd * previewHeight;
+		mRGBA = new int[mFrameSize];
+		mBitmap = Bitmap.createBitmap(previewWidtd, previewHeight, Bitmap.Config.ARGB_8888);
+	}
+
+	@Override
+	protected void onPreviewStopped() {
+		if(mBitmap != null) {
+			mBitmap.recycle();
+			mBitmap = null;
+		}
+		mRGBA = null;
+		
+		
+	}
+
     @Override
     protected Bitmap processFrame(byte[] data) {
-        int frameSize = getFrameWidth() * getFrameHeight();
-        int[] rgba = new int[frameSize];
+        int[] rgba = mRGBA;
 
         CircleObjectTrack(getFrameWidth(), getFrameHeight(), data, rgba, ObjTrackActivity.bShowTresholded);
 
-        Bitmap bmp = Bitmap.createBitmap(getFrameWidth(), getFrameHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap bmp = mBitmap; 
         bmp.setPixels(rgba, 0/* offset */, getFrameWidth() /* stride */, 0, 0, getFrameWidth(), getFrameHeight());
-        
         return bmp;
     }
 
     public native void CircleObjectTrack(int width, int height, byte yuv[], int[] rgba, boolean debug);
 
     static {
-        System.loadLibrary("objtrack_opencv_jni");
+    	System.loadLibrary("objtrack_opencv_jni");
     }
 }
