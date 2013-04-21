144789598e817fab14bb175e73562d009c688ef7
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2013-04-21 15:50:42 +0000

    [STM32 IDE] provision for millis()/delay_ms() on devices with higher clock rate

--- trunk/stm32/stm32-ide/hardware/cores/bsp/timer.h
+++ trunk/stm32/stm32-ide/hardware/cores/bsp/timer.h
@@ -11,8 +11,14 @@ void delay_ms(uint16_t cnt);
 
 // TIM15 - 1MHz
 #define micros()	((uint16_t)(TIM15->CNT))
-// TIM17 - 1kHz
-#define millis()	((uint16_t)(TIM17->CNT))
+
+#if defined (STM32F10X_LD_VL) || defined (STM32F10X_MD_VL) || (defined STM32F10X_HD_VL)
+  // TIM17 - 1kHz @ 24MHZ clock
+  #define millis()	((uint16_t)(TIM17->CNT))
+#else
+  // TIM17 - 2kHz @ 72MHZ clock
+  #define millis()	((uint16_t)(TIM17->CNT)>>1)
+#endif
 
 #ifdef __cplusplus
 }
