a680a39226ec43e0ad5d217826e7a70f8e9558bc
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2013-09-10 04:16:48 +0000

    [avr32-ide] added built-in counter/delays (e.g. "millis()")

--- trunk/AVR32/avr32-ide/hardware/cores/bsp/bsp_clock.h
+++ trunk/AVR32/avr32-ide/hardware/cores/bsp/bsp_clock.h
@@ -6,4 +6,14 @@
 #define TARGET_MCUCLK_FREQ_HZ 12000000  // MCU clock target frequency, in Hz
 #define TARGET_PBACLK_FREQ_HZ 12000000  // PBA clock target frequency, in Hz
 
+#include <cycle_counter/cycle_counter.h>
+
+
+#define micros()			cpu_cy_2_us( Get_sys_count(), TARGET_MCUCLK_FREQ_HZ )
+#define millis()			cpu_cy_2_ms( Get_sys_count(), TARGET_MCUCLK_FREQ_HZ )
+
+#define delay_us(cnt)		cpu_delay_us(cnt, TARGET_MCUCLK_FREQ_HZ)
+#define delay_ms(cnt)		cpu_delay_ms(cnt, TARGET_MCUCLK_FREQ_HZ)
+
+
 #endif	// __BSP_CLOCK_H__
