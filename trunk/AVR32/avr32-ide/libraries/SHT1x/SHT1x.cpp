030f30cec26b4ed6260b6633a79756534d9cb3d3
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2013-11-15 08:45:59 +0000

    [avr32-ide] initial SHT1x library

---
+++ trunk/AVR32/avr32-ide/libraries/SHT1x/SHT1x.cpp
@@ -0,0 +1,207 @@
+
+#include <SHT1x.h>
+#include <bsp_clock.h>
+extern "C"
+{
+  #include <gpio.h>
+}
+
+#define NOACK 0
+#define ACK   1
+                            //adr  command  r/w
+#define STATUS_REG_W 0x06   //000   0011    0
+#define STATUS_REG_R 0x07   //000   0011    1
+#define MEASURE_TEMP 0x03   //000   0001    1
+#define MEASURE_HUMI 0x05   //000   0010    1
+#define RESET        0x1e   //000   1111    0
+
+#define CLK_HIGH	gpio_set_pin_high(m_clkPin)
+#define CLK_LOW		gpio_set_pin_low(m_clkPin)
+#define CLK_OUTPUT	gpio_configure_pin(m_clkPin, GPIO_DIR_OUTPUT)
+
+#define DAT_HIGH	gpio_set_pin_high(m_datPin)
+#define DAT_LOW		gpio_set_pin_low(m_datPin)
+
+#define DAT_INPUT	gpio_configure_pin(m_datPin, GPIO_DIR_INPUT | GPIO_PULL_UP )
+#define DAT_OUTPUT	gpio_configure_pin(m_datPin, GPIO_DIR_OUTPUT | GPIO_OPEN_DRAIN | GPIO_PULL_UP | GPIO_INIT_HIGH)
+#define DAT_READ	gpio_get_pin_value(m_datPin)
+
+#define DELAY_PULSE	delay_us(5)
+#define DELAY_SETUP	delay_us(1)
+
+SHT1x::SHT1x(): m_datPin(0), m_clkPin(0)
+{
+}
+
+bool SHT1x::begin(uint32_t dataPin, uint32_t clockPin)
+{
+	if( !((dataPin<=AVR32_PIN_PA00 && dataPin>=AVR32_PIN_PA22) \
+		|| (dataPin<=AVR32_PIN_PB00 && dataPin>=AVR32_PIN_PB12))) return false;
+	if( !((clockPin<=AVR32_PIN_PA00 && clockPin>=AVR32_PIN_PA22) \
+		|| (clockPin<=AVR32_PIN_PB00 && clockPin>=AVR32_PIN_PB12))) return false;
+	
+	m_datPin = dataPin;
+	m_clkPin = clockPin;
+	
+	CLK_OUTPUT;
+	return true;
+}
+
+bool SHT1x::readTemperature(float *pConvertedValue, uint16_t *pRawValue, uint8_t *pChecksum)
+{
+/*
+  unsigned char error=0;
+  unsigned int i;
+
+  s_transstart();                   //transmission start
+  switch(mode){                     //send command to sensor
+    case TEMP	: error+=s_write_byte(MEASURE_TEMP); break;
+    case HUMI	: error+=s_write_byte(MEASURE_HUMI); break;
+    default     : break;	 
+  }
+  for (i=0;i<65535;i++) if(DATA==0) break; //wait until sensor has finished the measurement
+  if(DATA) error+=1;                // or timeout (~2 sec.) is reached
+  *(p_value)  =s_read_byte(ACK);    //read the first byte (MSB)
+  *(p_value+1)=s_read_byte(ACK);    //read the second byte (LSB)
+  *p_checksum =s_read_byte(noACK);  //read checksum
+  return error;
+*/
+	startTransmission();                   //transmission start
+	if(!writeByte(MEASURE_TEMP)) return false; //send command to sensor
+	DAT_INPUT;
+	DELAY_SETUP;
+	volatile unsigned long prev_millis = millis();
+	//wait until sensor has finished the measurement
+	while( (DAT_READ!=0) && ((unsigned long)(millis()-prev_millis)<2000) ) delay(1);
+	if(DAT_READ) return false; // timeout
+	
+	uint16_t raw = (readByte(ACK)<<8); // MSB
+	raw |= readByte(ACK); // LSB
+	if(pRawValue) *pRawValue = raw;
+	
+	uint8_t chk = readByte(NOACK);
+	if(pChecksum) *pChecksum = chk;
+	
+	return true;
+}
+
+bool SHT1x::reset(void)
+{
+	// resets the sensor by a softreset 
+	resetConnection(); //reset communication
+	return writeByte(RESET); //send RESET-command to sensor
+}
+
+bool SHT1x::readStatusReg(uint8_t *pvalue, uint8_t *pchecksum)
+{
+// reads the status register with checksum (8-bit)
+	startTransmission();             //transmission start
+	if(!writeByte(STATUS_REG_R)) return false; //send command to sensor
+	*pvalue = readByte(ACK);       //read status register (8-bit)
+	*pchecksum = readByte(NOACK);  //read checksum (8-bit)  
+	return true;
+}
+
+bool SHT1x::writeStatusReg(uint8_t *pvalue)
+{
+// writes the status register with checksum (8-bit)
+	startTransmission();                   //transmission start
+	if(!writeByte(STATUS_REG_W)) return false;//send command to sensor
+	if(!writeByte(*pvalue)) return false;    //send value of status register
+	return true;
+}
+
+//===============================================================================
+
+bool SHT1x::writeByte(uint8_t value)
+{
+	DAT_OUTPUT;
+	for (uint8_t i=0x80; i>0; i>>=1)
+	{
+		if (i & value) DAT_HIGH;
+		else DAT_LOW;
+		DELAY_SETUP;
+		CLK_HIGH;
+		DELAY_PULSE;
+		CLK_LOW;
+		DELAY_SETUP;
+	}
+	DAT_INPUT;
+	DELAY_SETUP;
+	CLK_HIGH;
+	DELAY_PULSE;
+	bool error = (DAT_READ!=0);
+	CLK_LOW;
+	return error;
+}
+
+uint8_t SHT1x::readByte(bool ack)
+{
+	uint8_t val = 0;
+	DAT_INPUT;
+	for (uint8_t i=0x80; i>0; i>>=1)
+	{
+		CLK_HIGH;
+		DELAY_PULSE;
+		if(DAT_READ)
+			val |= i;
+		CLK_LOW;
+		DELAY_SETUP;
+	}
+	DAT_OUTPUT;
+	DELAY_SETUP;
+	if(ack) DAT_LOW;
+	else DAT_HIGH;
+	DELAY_SETUP;
+	CLK_HIGH;
+	DELAY_PULSE;
+	CLK_LOW;
+	DELAY_SETUP;
+	DAT_INPUT;
+	return val;
+}
+
+void SHT1x::startTransmission()
+{
+// generates a transmission start 
+//       _____         ________
+// DATA:      |_______|
+//           ___     ___
+// SCK : ___|   |___|   |______
+	DAT_OUTPUT;
+	DAT_HIGH; CLK_LOW;
+	DELAY_SETUP;
+	CLK_HIGH;
+	DELAY_SETUP;
+	DAT_LOW;
+	DELAY_SETUP;
+	CLK_LOW;
+	DELAY_PULSE;
+	CLK_HIGH;
+	DELAY_SETUP;
+	DAT_HIGH;
+	DELAY_SETUP;
+	CLK_LOW;
+}
+
+void SHT1x::resetConnection()
+{
+// communication reset: DATA-line=1 and at least 9 SCK cycles followed by transstart
+//       _____________________________________________________         ________
+// DATA:                                                      |_______|
+//          _    _    _    _    _    _    _    _    _        ___     ___
+// SCK : __| |__| |__| |__| |__| |__| |__| |__| |__| |______|   |___|   |______
+	DAT_OUTPUT;
+	DAT_HIGH; CLK_LOW;
+	DELAY_SETUP;
+	for(uint8_t i=0; i<9; i++)
+	{
+		CLK_HIGH; DELAY_PULSE;
+		CLK_LOW; DELAY_SETUP;
+	}
+	startTransmission();
+}
+
+
+
+
