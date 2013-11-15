6d2578ab9e2b6a5a49a42d8982ec1595473b5432
yus.projectproto <yus.projectproto@gmail.com>
2013-11-15 08:45:59 +0000

    [avr32-ide] initial SHT1x library

---
+++ AVR32/avr32-ide/libraries/SHT1x/SHT1x.h
@@ -0,0 +1,29 @@
+
+#ifndef SHT1X_X
+#define SHT1X_X
+
+#include <avr32/io.h>
+#include <compiler.h>
+
+class SHT1x
+{
+public:
+	SHT1x();
+	bool begin(uint32_t dataPin, uint32_t clockPin);
+	
+	bool readTemperature(float *pConvertedValue, uint16_t *pRawValue=NULL, uint8_t *pChecksum=NULL);
+	
+	bool reset();
+	bool readStatusReg(uint8_t *pvalue, uint8_t *pchecksum);
+	bool writeStatusReg(uint8_t *pvalue);
+	
+private:
+	uint32_t m_datPin;
+	uint32_t m_clkPin;
+	bool writeByte(uint8_t value);
+	uint8_t readByte(bool ack);
+	void startTransmission();
+	void resetConnection();
+};
+
+#endif //SHT1X_X
