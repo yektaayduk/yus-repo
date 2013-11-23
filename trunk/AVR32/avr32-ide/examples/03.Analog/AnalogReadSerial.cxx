c44969748c5503c0b0a1174009a075b5ebc96444
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2013-11-23 04:38:11 +0000

    [avr32-ide] fix analogRead() function

--- trunk/AVR32/avr32-ide/examples/03.Analog/AnalogReadSerial.cxx
+++ trunk/AVR32/avr32-ide/examples/03.Analog/AnalogReadSerial.cxx
@@ -2,7 +2,7 @@
 
 // available ADC pins on UC3L0128
 // PA14, PA15, PA16, PA18, PA19, PB06, PB07 & PB08
-#define ADC_PIN PA14
+#define ADC_PIN PB07
 
 int main(void)
 {
@@ -12,8 +12,8 @@ int main(void)
 	{
 		// read the input on adc pin
 		uint32_t adcval = analogRead(ADC_PIN);
-		// print out the value
-		Serial.print("adc val = %d\r\n", adcval);
+		// print out the value and equivalent voltage (@3.3V supply)
+		Serial.print("ADC value = %d (%d mV)\r\n", adcval, adcval*3300/4095);
 		delay(1000);
 	}
 }
