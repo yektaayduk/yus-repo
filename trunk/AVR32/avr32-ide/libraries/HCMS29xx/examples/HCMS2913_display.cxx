635e6eeb6369f9eb5d4a810f6fabdc25538a71e4
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2013-11-26 09:56:38 +0000

    [avr32-ide] initial HCMS-29xx (led matrix display) library

---
+++ trunk/AVR32/avr32-ide/libraries/HCMS29xx/examples/HCMS2913_display.cxx
@@ -0,0 +1,19 @@
+#include <avr32.h>
+#include <HCMS29xx.h>
+
+HCMS29xx myLed;
+const char message[] = "    HCMS-2913 tested w/ AT32UC3L    ";
+
+int main(void)
+{
+	myLed.begin(PB02 /*data*/, PB03 /*clock*/,
+		PB04 /*rs*/, PB05 /*ce*/, PB06 /*reset*/);
+	
+	myLed.displayMessage("HCMS2913");
+	delay(5000);
+	
+	while(true)
+	{
+		myLed.scrollMessage(message, sizeof(message));
+	}
+}
