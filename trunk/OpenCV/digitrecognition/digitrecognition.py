8be7b92a2ab06f77dfcb107c6d4dddf832a8734c
yus.projectproto@gmail.com <yus.projectproto@gmail.com@8c661e88-4e0d-4e43-2abf-18b7afe299bb>
2014-07-18 02:38:23 +0000

    import python-opencv digit recognition with a given truetype font file. based on http://stackoverflow.com/questions/9413216/simple-digit-recognition-ocr-in-opencv-python

---
+++ trunk/OpenCV/digitrecognition/digitrecognition.py
@@ -0,0 +1,66 @@
+
+import cv2
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw, ImageOps
+
+# create training model based on the given TTF font file
+def createDigitsModel(fontfile, digitwidth, digitheight):
+    font = ImageFont.truetype(fontfile, digitheight)
+    samples =  np.empty((0,digitwidth*digitheight))
+    responses = []
+    for n in range(10):
+        pil_im = Image.new("RGB", (digitwidth*2, digitheight*2))
+        ImageDraw.Draw(pil_im).text((0, 0), str(n), font=font)
+        pil_im = pil_im.crop(pil_im.getbbox())
+        pil_im = ImageOps.invert(pil_im)
+        #pil_im.save(str(n) + ".png")
+
+        # convert to cv image
+        cv_image = cv2.cvtColor(np.array( pil_im ), cv2.COLOR_RGBA2BGRA)
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray,(5,5),0)
+        thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
+
+        roi = cv2.resize(thresh,(digitwidth,digitheight))
+        responses.append( n )
+        sample = roi.reshape((1,digitwidth*digitheight))
+        samples = np.append(samples,sample,0)
+
+    samples = np.array(samples,np.float32)
+    responses = np.array(responses,np.float32)
+    
+    model = cv2.KNearest()
+    model.train(samples,responses)
+    return model
+
+# digit recognition part
+def findDigits(imagefile, digitwidth, digitheight, fontfile="C:\\Windows\\Fonts\\Arial.ttf"):
+    im = cv2.imread(imagefile)
+    out = np.zeros(im.shape,np.uint8)
+    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+
+    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2) # bug?!
+
+    model = createDigitsModel(fontfile, digitwidth, digitheight)
+    for cnt in contours:
+        [x,y,w,h] = cv2.boundingRect(cnt)
+        if  h>digitheight and w>digitwidth/4:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),1)
+            roi = thresh[y:y+h,x:x+w] # crop
+            roi = cv2.resize(roi,(digitwidth,digitheight))
+            roi = roi.reshape((1,digitwidth*digitheight))
+            roi = np.float32(roi)
+            retval, results, neigh_resp, dists = model.find_nearest(roi, k=1)
+            string = str(int((results[0][0])))
+            cv2.putText(out,string,(x,y+h),0,1,(0,255,0))
+
+    cv2.imshow('in',im)
+    cv2.imshow('out',out)
+    cv2.waitKey(0)
+    cv2.destroyWindow( 'in' )
+    cv2.destroyWindow( 'out' )
+
+findDigits('pi.png', 20, 28)
+print 'done.'
